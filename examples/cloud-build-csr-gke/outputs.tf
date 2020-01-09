4494f9665e7300452b597647e33de804fe1c4df9
Rob Morgan <robbym@gmail.com>
2020-01-09 12:04:44 +0800

    output the build trigger id

--- examples/cloud-build-csr-gke/outputs.tf
+++ examples/cloud-build-csr-gke/outputs.tf
@@ -1,3 +1,8 @@
+output "trigger_id" {
+  description = "The unique identifier for the Cloud Build trigger."
+  value       = google_cloudbuild_trigger.cloud_build_trigger.trigger_id
+}
+
 output "repository_http_url" {
   description = "HTTP URL of the repository in Cloud Source Repositories."
   value       = google_sourcerepo_repository.repo.url
